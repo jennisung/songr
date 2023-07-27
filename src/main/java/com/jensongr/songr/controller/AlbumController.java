066796bb55b4118607406527b8d11f29fa9a6e97
Jennifer Sung <sung.jenni93@gmail.com>
2023-07-26 21:16:56 -0400

    add song model

--- src/main/java/com/jensongr/songr/controller/AlbumController.java
+++ src/main/java/com/jensongr/songr/controller/AlbumController.java
@@ -39,27 +39,13 @@ public class AlbumController {
         albumRepository.save(album);
         return "redirect:/albums";
     }
+
+
+
+    //lab 13
+
+
+
 }
 
 
-
-
-//three albums
-//import com.jensongr.songr.model.Album;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class AlbumController {
-//    @GetMapping("/albums")
-//    public String albums(Model model) {
-//        Album[] albums = {
-//                new Album("Channel Orange", "Frank Ocean", 17, 62 * 60 + 18, "channel_orange.jpeg"),
-//                new Album("AM", "Arctic Monkeys", 12, 41 * 60 + 38, "am.jpg"),
-//                new Album("Sawayama", "Rina Sawayama", 13, 43 * 60, "sawayama.png")
-//        };
-//        model.addAttribute("albums", albums);
-//        return "albums.html";
-//    }
-//}
---
+++ src/main/java/com/jensongr/songr/model/Song.java
@@ -0,0 +1,68 @@
+package com.jensongr.songr.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private int songLength;
+    private int trackNumber;
+
+    @ManyToOne
+    private Album album;
+
+    public Song() {
+    }
+
+    public Song(String title, int songLength, int trackNumber, Album album) {
+        this.title = title;
+        this.songLength = songLength;
+        this.trackNumber = trackNumber;
+        this.album = album;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSongLength() {
+        return songLength;
+    }
+
+    public void setSongLength(int songLength) {
+        this.songLength = songLength;
+    }
+
+    public int getTrackNumber() {
+        return trackNumber;
+    }
+
+    public void setTrackNumber(int trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+}
